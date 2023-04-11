afa9fffea61bf7dad0e7e59e0f70f44561e38ea5
Maharshi Dhori <dhorimaharshi@gmail.com>
2023-04-11 11:20:08 +0530

    modified header file

--- header.php
+++ header.php
@@ -15,19 +15,19 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
-          <?php
+          <!-- <?php
             wp_nav_menu( array(
               'theme_location' => 'headerMenuLocation'
             ));
-          ?>
+          ?> -->
 
-          <!-- <ul>
-              <li><a href="<?php echo site_url('/about-us' )?>">About Us</a></li>
-              <li><a href="#">Programs</a></li>
+          <ul>
+              <li <?php if(is_page('about-us') or wp_get_post_parent_id( 0 ) == 14) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us' )?>">About Us</a></li>
+              <li><a href="#">Programs</a></li> 
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
-            </ul> -->
+            </ul>
 
           </nav>
           <div class="site-header__util">
