ed9cca0a7fd80d4925a5e2786a6b25b9bd42f8dd
Maharshi Dhori <dhorimaharshi@gmail.com>
2023-09-05 12:53:21 +0530

    [ADD/UPDATE] Updated code to display page banner,tile and subtitle dynamically

--- functions.php
+++ functions.php
@@ -1,58 +1,95 @@
-<?php 
-    function university_files(){
-        wp_enqueue_script('university-main-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-        wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-        wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-        wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
-        wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+<?php
+function pageBanner($args = null)
+{
+    if (!isset($args['title'])) {
+        $args['title'] = get_the_title();
     }
 
-    add_action('wp_enqueue_scripts', 'university_files');
-
-
-    function university_features(){
-        register_nav_menu('headerMenuLocation', 'Header Menu Location');
-        register_nav_menu('footerLocationOne', 'Footer Location One');
-        register_nav_menu('footerLocationTwo', 'Footer Location Two');
-        add_theme_support('title-tag');
-        add_theme_support( 'post-thumbnails' );
-        add_image_size('professorLandscape', 400, 260, true);
-        add_image_size('professorPortrait', 480, 650, true);    
+    if (!isset($args['subtitle'])) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
     }
 
-    add_action('after_setup_theme', 'university_features');
-
-    
-    function university_adjust_queries($query){
-        if(!is_admin() and is_post_type_archive('programs') and is_main_query()) {
-            $query->set('order_by', 'title');
-            $query->set('order', 'ASC');
-            $query->set('posts_per_page', -1);
-        }
-
-
-        if(!is_admin() and is_post_type_archive('events') and $query->is_main_query()){
-            $today = date('Ymd');
-            $query->set( 'meta_key', 'event_date');
-            $query->set( 'orderby', 'meta_value_num');
-            $query->set( 'order', 'ASC');
-            $query->set( 'meta_query', array(
-                array(
-                  'key' => 'event_date',
-                  'compare' => '>=',
-                  'value' => $today,
-                  'type' => 'numeric'
-                )
-              ));
+    if (!isset($args['photo'])) {
+        if (get_field('page_banner_image')) {
+            $args['photo'] = get_field('page_banner_image')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('images/ocean.jpg');
         }
     }
+?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle'] ?></p>
+            </div>
+        </div>
+    </div>
+
+<?php
+}
+
+
+function university_files()
+{
+    wp_enqueue_script('university-main-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+
+function university_features()
+{
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
+}
+
+add_action('after_setup_theme', 'university_features');
+
+
+function university_adjust_queries($query)
+{
+    if (!is_admin() and is_post_type_archive('programs') and is_main_query()) {
+        $query->set('order_by', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
+
+    if (!is_admin() and is_post_type_archive('events') and $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
+    }
+}
 add_action('pre_get_posts', 'university_adjust_queries');
 
 
 add_filter('embed_oembed_html', function ($html, $url, $attr, $post_id) {
- if(strpos($html, 'linked.com') !== false || strpos($html, 'youtu.be') !== false){
- return '<div class="responsive-embed-container embed-responsive-16by9">' . $html . '</div>';
- } else {
- return $html;
- }
+    if (strpos($html, 'linked.com') !== false || strpos($html, 'youtu.be') !== false) {
+        return '<div class="responsive-embed-container embed-responsive-16by9">' . $html . '</div>';
+    } else {
+        return $html;
+    }
 }, 10, 4);
--- single-professor.php
+++ single-professor.php
@@ -1,17 +1,10 @@
 <?php
 get_header();
 while (have_posts()) {
-    the_post(); ?>
+    the_post();
+    pageBanner();
+?>
 
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
-            <div class="page-banner__intro">
-                <p>DON'T FORGET TO REPLACE ME LATER.</p>
-            </div>
-        </div>
-    </div>
 
     <div class="container container--narrow page-section">
 
@@ -27,20 +20,20 @@ while (have_posts()) {
         </div>
 
         <hr class="section-break">
-            <h2 class="headline headline--medium">Subject(s) Taught</h2>
-            <ul class="link-list min-list">
-        <?php
-        $relatedPrograms = get_field('related_programs');
+        <h2 class="headline headline--medium">Subject(s) Taught</h2>
+        <ul class="link-list min-list">
+            <?php
+            $relatedPrograms = get_field('related_programs');
 
-        foreach ($relatedPrograms as $programs) { ?>
-            
-            <li><a href="<?php echo get_the_permalink($programs) ?>"><?php echo  get_the_title($programs) ?></a></li>
-        
-        <?php
-        }
+            foreach ($relatedPrograms as $programs) { ?>
 
-        ?>
-      </ul>
+                <li><a href="<?php echo get_the_permalink($programs) ?>"><?php echo  get_the_title($programs) ?></a></li>
+
+            <?php
+            }
+
+            ?>
+        </ul>
     </div>
 <?php
 }
